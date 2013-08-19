c606e615168bb82c2c1131cce57806d691037801
balazs <vargabalazslaszlo@gmail.com>
2013-08-19 12:10:15 -0700

    crontest add

---
+++ protected/commands/defaultCommand.php
@@ -0,0 +1,27 @@
+<?php
+// Hívása: ./yiic_dev Default test --verbose=0 --test_only=0
+class DefaultCommand extends CConsoleCommand
+{
+    public $verbose=false;
+    public $test_only=false;
+
+
+    /**
+     * Initializes the command object.
+     * This method is invoked after a command object is created and initialized with configurations.
+     * You may override this method to further customize the command before it executes.
+     * @since 1.1.6
+     */
+    public function init()
+    {
+        
+    }
+
+    public function actionTest(){
+    	$ct = new CronTest();
+    	$ct->datetime = date('Y-m-d H:i:s');
+    	$ct->save();
+    	
+    	
+    }
+}
